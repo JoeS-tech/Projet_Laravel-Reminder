009fb869ae4ae7067f5492ef4c494aa5ce0e18ef
Joseph S <joe.pm.pro@gmail.com>
2020-11-05 15:24:12 +0100

    modif profile

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -1,52 +1,61 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="card-body">
+<div class="card-body text-center">
 
     @if(Auth::user()->lastname ==NULL && Auth::user()->firstname ==NULL)
-        Saisissez votre nom et prénom
+        <h2>Bonjour!</h2>
         <br>
         @else
-        <span>Prénom: {{Auth::user()->firstname}} </span>
-        <br>
-        <span>Nom: {{Auth::user()->lastname}} </span>
+        <span><h2>Bonjour {{Auth::user()->firstname}}
+         {{Auth::user()->lastname}} !</h2></span>
         <br>
     @endif
 
-    <span>Username: {{Auth::user()->name}}</span>
+    <span><h5><b>Psuedo:</b> {{Auth::user()->name}}</h5></span>
     <br>
-    <span>Email: {{Auth::user()->email}}</span>
+    <span><h5><b>Email:</b> {{Auth::user()->email}}</h5></span>
 
-{{-- @if("isset" ) --}}
-    <input type="button" name="cmd" value="Modifiez votre Profil">
 </div>
 
-<form method="POST" action="{{ @route('user.sendProfile') }}" enctype="multipart/form-data">
-    @if($errors->any()) {{-- si il y a une erreur...--}}
-        @foreach($errors->all() as $e) {{-- --}}
-            <h3 class="red-text">{{ $e }}</h3>
-        @endforeach
-    @endif
-    @csrf
-    <label for="file">Séléctionnez votre avatar! </label>
-    <input type="file" name="avatar" id="file">
-    <br>
-    <label for="name">Entrez un nouveau pseudo: </label>
-    <input type="text" name="name" id="name" value="{{Auth::user()->name}}">
-    <br>
-    <label for="email">Entrez votre email: </label>
-    <input type="text" name="email" id="email" value="{{Auth::user()->email}}">
-    <br>
-    <label for="firstname">Entrez votre prénom: </label>
-    <input type="text" name="firstname" id="firstname" placeholder="prénom">
-    <br>
-    <label for="lastname">Entrez votre nom: </label>
-    <input type="text" name="lastname" id="lastname" placeholder="nom">
-    <br>
-    <label for="password">Entrez un nouveau mot de passe: </label>
-    <input type="password" name="password" id="password" placeholder="mot de passe">
-    <br>
-    <input type="submit" value="Valider">
+{{-- @if("isset" ) --}}
 
-</form>
+<button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+    Modifiez votre Profile
+  </button>
+</p>
+<div class="collapse" id="collapseExample">
+  <div class="card card-body">
+    <form class="formulaire_profile" method="POST" action="{{ @route('user.sendProfile') }}" enctype="multipart/form-data">
+        @if($errors->any()) {{-- si il y a une erreur...--}}
+            @foreach($errors->all() as $e) {{-- --}}
+                <h3 class="red-text">{{ $e }}</h3>
+            @endforeach
+        @endif
+        @csrf
+        <label for="file">Séléctionnez votre avatar! </label>
+        <input type="file" name="avatar" id="file">
+        <br>
+        <label for="name">Entrez un nouveau pseudo: </label>
+        <input type="text" name="name" id="name" value="{{Auth::user()->name}}">
+        <br>
+        <label for="email">Entrez votre email: </label>
+        <input type="text" name="email" id="email" value="{{Auth::user()->email}}">
+        <br>
+        <label for="firstname">Entrez votre prénom: </label>
+        <input type="text" name="firstname" id="firstname" placeholder="prénom">
+        <br>
+        <label for="lastname">Entrez votre nom: </label>
+        <input type="text" name="lastname" id="lastname" placeholder="nom">
+        <br>
+        <label for="password">Entrez un nouveau mot de passe: </label>
+        <input type="password" name="password" id="password" placeholder="mot de passe">
+        <br>
+        <input type="submit" value="Valider">
+
+    </form>
+  </div>
+</div>
+
 @endsection
+
