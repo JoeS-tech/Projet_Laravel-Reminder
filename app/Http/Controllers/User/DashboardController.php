178dd62e42dd3a16b2dc314811d2e1cdbfa03555
Amaury <amaury.meugnier@hotmail.fr>
2020-11-05 15:09:43 +0100

    Test scinder page users

--- app/Http/Controllers/User/DashboardController.php
+++ app/Http/Controllers/User/DashboardController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
+use App\Models\User;
+
 // use App\Models\User;
 
 class DashboardController extends Controller
@@ -13,16 +15,29 @@ class DashboardController extends Controller
     {
         $this->middleware('auth');
     }
-    public function dashboard(Request $request, $id)
+    public function dashboard(Request $request, $id = null)
     {
-        $id = Auth::user();
-        $id = $id->id;
+
+        // $id = Auth::user()->where;
+        // $id = $id->id;
+
+        // dd($id);
+        // $id = Auth::where(function ($query) {
+        //     $query
+        //         ->select('id')
+        //         ->from('membership')
+        //         ->whereColumn('user_id', 'users.id')
+        //         ->orderByDesc('start_date')
+        //         ->limit(1);
+        // }, 'Pro')->get();
         // dd($id);
         // $user = $request->user();
         // $user = $user->id;
         //
-        // $id = auth()->id();
-        // $profile = User::find($id);
-        return view('user.dashboard', [$id]);
+        $id = auth()->id();
+        $id = User::find($id);
+        $id = $id->id;
+        // dd($id);
+        return view('user.dashboard');
     }
 }
