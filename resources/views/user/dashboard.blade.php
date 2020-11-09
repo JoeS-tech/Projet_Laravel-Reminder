7da32f832d908990a642b98f7a2f988c86dc5b42
Amaury <amaury.meugnier@hotmail.fr>
2020-11-09 21:23:24 +0100

    css

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -3,6 +3,10 @@
     background-image: url('storage/assets/uploads/robibi.jpg');
     background-position: center center;
     }
+    .lsh_delicon{
+        width:30px;
+        border-radius: 50%;
+    }
 </style>
 
 @extends('layouts.app')
@@ -11,36 +15,43 @@
 
 
 <div>
-    <h3>Ajouter une Nouvelle Table:</h3>
-    <form method="POST" action="{{ @route('user.sendTable') }}">
-        @csrf
-        <input type="text" name="title" value="Nouvelle Table">
-        <input type="submit" value="Envoyer">
-    </form>
-</div>
-<div>
-    <br>
-    @foreach ($tables as $table)
-        <div>
-        <a href="{{ @route('user.postit',[$table->id]) }}">{{ $table->title }}<br></a>
-        {{ $table->id }}
-
-        <form method="POST" action="{{ @route('ediTable',[$table->id]) }}">
-            @csrf
-
-            <input type="text" name="title" value="Changer le Titre">
-            <input type="hidden" name="test" value='{{$table->id}}'>
-            <input type="submit" value='modifier'>
-
-        </form>
-        <form method="POST" action="{{ @route('delTable',[$table->id]) }}">
-            @csrf
-            <input type="submit" value='Supprimer'>
-
-        </form>
-
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">Ajouter une Nouvelle Table:</h5>
+            <form method="POST" action="{{ @route('user.sendTable') }}">
+                @csrf
+                <input type="text" name="title" value="Nouvelle Table">
+                <input type="submit" value="Envoyer">
+            </form>
         </div>
-    @endforeach
+    <div>
+</div>
+</div>
+
+@foreach ($tables as $table)
+        <div>
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+
+                     <h3 class="card-title"><a href="{{ @route('user.postit',[$table->id]) }}">
+                        {{ $table->title }}</a></h3>
+
+                    <form method="POST" action="{{ @route('ediTable',[$table->id]) }}">
+                        @csrf
+                        <input type="text" name="title" value="Changer le Titre">
+                        <input type="hidden" name="test" value='{{$table->id}}'>
+                        <input type="submit" value='modifier'>
+                    </form>
+
+                    <form method="POST" action="{{ @route('delTable',[$table->id]) }}">
+                        @csrf
+                        <input class="lsh_delicon"type="image" name="delTab"
+                        src="../storage/assets/uploads/del.png" alt="iconDel">
+                    </form>
+
+                </div>
+        @endforeach
     </div>
+</div>
 
 @endsection
