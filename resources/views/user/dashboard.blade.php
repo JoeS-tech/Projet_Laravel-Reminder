b3ebb6da67ea6fad8795636089ff1011e1840cc0
Amaury <amaury.meugnier@hotmail.fr>
2020-11-09 22:30:50 +0100

    css suite

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -9,6 +9,10 @@
         width:30px;
         border-radius: 50%;
     }
+    /* .lsh_deliconModal{
+        width:25%;
+        border-radius: 50%;
+    } */
 </style>
 @extends('layouts.app')
 
@@ -35,17 +39,32 @@
 
                      <h3 class="card-title"><a href="{{ @route('user.postit',[$table->id]) }}">
                         {{ $table->title }}</a></h3>
+                    {{-- <div class="modal-dialog modal-sm"> --}}
 
-                    <form method="POST" action="{{ @route('ediTable',[$table->id]) }}">
-                        @csrf
-                        <input type="text" name="title" value="Changer le Titre">
-                        <input type="hidden" name="test" value='{{$table->id}}'>
-                        <input type="submit" value='modifier'>
-                    </form>
 
+                    <!-- Small modal for EDIT -->
+                    <input type="image" class="lsh_delicon" data-toggle="modal"
+                    src="../storage/assets/uploads/edit.png" alt="iconDel"
+                    data-target={{ '.modalComment'.$table->id }}>
+
+                    <div class="modal fade {{ 'modalComment'.$table->id }} " tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-sm">
+                            <div class="modal-content">
+                            <form method="POST" action="{{ @route('ediTable',[$table->id]) }}">
+                                @csrf
+                                <input type="text" name="title" value="Changer le Titre">
+                                <input type="hidden" name="test" value='{{$table->id}}'>
+                                <input type="submit" value='modifier'>
+                            </form>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    {{-- </div> --}}
                     <form method="POST" action="{{ @route('delTable',[$table->id]) }}">
                         @csrf
-                        <input class="lsh_delicon"type="image" name="delTab"
+                        <input class="lsh_delicon" type="image" name="delTab"
                         src="../storage/assets/uploads/del.png" alt="iconDel">
                     </form>
 
@@ -53,5 +72,6 @@
         @endforeach
     </div>
 </div>
+
 
 @endsection
